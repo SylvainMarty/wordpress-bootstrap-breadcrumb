d766944fa9139c87f8188cd0a40ab98dc2324fe5
Danny Trunk <dtrunk90@gmail.com>
2020-01-04 22:05:05 +0100

    Update bootstrap-breadcrumb.php
    even more configurable

--- bootstrap-breadcrumb.php
+++ bootstrap-breadcrumb.php
@@ -15,7 +15,8 @@ $defaults = array(
 	'custom_home_icon' => false, // Insert html tag (like Fontawesome <i class="fa fa-xx"></li>)
 	'custom_post_types' => false, // Prevent custom post types with hierarchical structure
 	'do_not_show_on_index' => true,
-	'item_classes' => ''
+	'item_classes' => '',
+	'show_post_type' => true
 );
 
 /**
@@ -32,14 +33,16 @@ function bootstrap_breadcrumb($options) {
 		echo '<ol class="breadcrumb ' . $config['breadcrumb_classes'] . '">';
 		if ($config['before_items'] !== '')
 			echo $config['before_items'];
-		echo '<li class="breadcrumb-item ' . $config['item_classes'] . '"><a href="';
-		echo get_option('home');
-		echo '">';
+		echo '<li class="breadcrumb-item ' . $config['item_classes'] . ' ' . (is_front_page() || is_home() ? 'active' : '') . '">';
+		if (!is_front_page() && !is_home())
+			echo '<a href="' . get_option('home') . '">';
 		if( $config['custom_home_icon'] )
 			echo $config['custom_home_icon'];
 			else
 				bloginfo('name');
-		echo "</a></li>";
+		if (!is_front_page() && !is_home())
+			echo "</a>";
+		echo "</li>";
 		if ( has_category() ) {
 			echo '<li class="breadcrumb-item ' . $config['item_classes'] . ' active"><a href="'.esc_url( get_permalink( get_page( get_the_category($post->ID) ) ) ).'">';
 			the_category(', ');
@@ -49,7 +52,8 @@ function bootstrap_breadcrumb($options) {
 			if ( is_category() )
 				echo '<li class="breadcrumb-item ' . $config['item_classes'] . ' active"><a href="'.esc_url( get_permalink( get_page( get_the_category($post->ID) ) ) ).'">'.get_the_category($post->ID)[0]->name.'</a></li>';
 			if ( $is_custom_post ) {
-				echo '<li class="breadcrumb-item ' . $config['item_classes'] . ' active"><a href="'.get_option('home').'/'.get_post_type_object( get_post_type($post) )->name.'">'.get_post_type_object( get_post_type($post) )->label.'</a></li>';
+				if ($config['show_post_type'])
+					echo '<li class="breadcrumb-item ' . $config['item_classes'] . ' active"><a href="'.get_option('home').'/'.get_post_type_object( get_post_type($post) )->name.'">'.get_post_type_object( get_post_type($post) )->label.'</a></li>';
 				if ( $post->post_parent ) {
 					$home = get_page(get_option('page_on_front'));
 					for ($i = count($post->ancestors)-1; $i >= 0; $i--) {
@@ -63,7 +67,7 @@ function bootstrap_breadcrumb($options) {
 					}
 				}
 			}
-			if ( is_single() )
+			if ( is_single() || is_page() )
 				echo '<li class="breadcrumb-item ' . $config['item_classes'] . ' active">'.get_the_title($post->ID).'</li>';
 		} elseif ( is_page() && $post->post_parent ) {
 			$home = get_page(get_option('page_on_front'));
